14d9302b42ebcc8944c156bbbf3f5d2fdaf39e1c
Lars Ole Simonsen <los@sirenia.eu>
2020-10-17 21:58:17 +0200

    add script for republishing the docker image

---
+++ scripts/update-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# TODO: could perhaps shrink the final image by using a separate image for building and a separate image for deploying
+# without the toolchain dependencies
+
+docker login
+docker build . -t sonos-tannoy:latest
+docker tag sonos-tannoy larsim/sonos-tannoy:latest
+docker push larsim/sonos-tannoy:latest
